7e0ce68a1a7bf759fcb7162e0e73bdb27ff784ba
WenhaoYou1 <ericwyou1@gmail.com>
2025-07-06 16:54:53 -0400

    update exception

---
+++ src/main/java/com/meng/orderservice/services/NotUpdatedException.java
@@ -0,0 +1,6 @@
+package com.meng.orderservice.services;
+
+public class NotUpdatedException extends Exception {
+    public NotUpdatedException() {}
+    public NotUpdatedException(String message) {super(message);}
+}
